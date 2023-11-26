489ad89f7176cde3467dc3bc5abcf1216970f29e
Audrius Molis <vienas00@gmail.com>
2023-11-26 12:28:19 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# clojure-course
+
+FIXME: description
+
+## Usage
+
+    $ lein run
+
+or
+
+    $  lein run 'Prague,Madrid,family'
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-course "0.1.1"
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot flights.search-engine
+  :target-path "target/%s")
